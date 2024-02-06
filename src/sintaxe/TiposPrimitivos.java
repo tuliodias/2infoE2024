3354f78c10e6c9cb26665052a150d512dbe35551
Tulio Marcos Dias da Silva <tuliodias@yahoo.com.br>
2024-02-06 12:02:41 -0200

    commit 2 info F

--- src/sintaxe/TiposPrimitivos.java
+++ src/sintaxe/TiposPrimitivos.java
@@ -10,5 +10,27 @@ package sintaxe;
  * @author tulio
  */
 public class TiposPrimitivos {
-    
+
+    public static void main(String[] args) {
+        byte idade = 127;
+        short nPais = 32767;
+        int nAluno = -2147483648;
+        long nHabitante = 1000000000000000000L;
+
+        float salario = 1000.35f;
+        double totalSalario = 1009000.35;
+
+        boolean opcao = false;//true
+        char sexo = 'F';
+
+        System.out.println("Byte: " + idade);
+        System.out.println("Short: " + nPais);
+        System.out.println("Int: " + nAluno);
+        System.out.println("Long: " + nHabitante);
+        System.out.println("Float: " + salario);
+        System.out.println("Double: " + totalSalario);
+        System.out.println("Boolean: " + opcao);
+        System.out.println("Char: " + sexo);
+    }
+
 }
